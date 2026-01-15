03b35b4a8fdddce376b88ed58129d27ce8659e79
sum-rg-blip <sumayamoha567@gmail.com>
2026-01-15 20:34:39 +0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,30 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+
+// Dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+// Customers
+Route::get('/customers', function () {
+    return view('customers');
+})->name('customers');
+
+// Messages
+Route::get('/messages', function () {
+    return view('messages');
+})->name('messages');
+
+// Order details
+Route::get('/order_details', function () {
+    return view('order_details');
+})->name('order.details');
